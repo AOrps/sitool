d754287605de130309c65f6586ad51ff1974f5f3
aorps <aorbeandrews@gmail.com>
2024-01-18 21:49:20 -0500

    moving things around
    Signed-off-by: aorps <aorbeandrews@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"log"
+
+	
+	"github.com/urfave/cli/v2"
+)
+  
+
+
+
 func main() {
+	app := &cli.App{
+		EnableBashCompletion: true,
+		Name: "greet",
+		Usage: "fight the loneliness! tut",
+		Action: func(*cli.Context) error {
+			fmt.Println("sup")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 
 }
